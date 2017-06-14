f461456855cc7b2a22248c4c16937948371018e7
Christiansen MEREL <christiansen.merel@gmail.com>
2017-06-14 12:03:49 +0200

    chore: corrige hostname

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ env
 
 set -x
 
-sshpass -e scp target/*.jar vivatech@http://vivatech.pintade.org:/home/vivatech/
-sshpass -e scp run.sh vivatech@http://vivatech.pintade.org:/home/vivatech/
+sshpass -e scp target/*.jar vivatech@vivatech.pintade.org:/home/vivatech/
+sshpass -e scp run.sh vivatech@vivatech.pintade.org:/home/vivatech/
 sshpass -e ssh vivatech@vivatech.pintade.org ./run.sh
