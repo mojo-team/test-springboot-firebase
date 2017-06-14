a1832d84795940730464035e4eefa5dd4c367a88
Christiansen MEREL <christiansen.merel@gmail.com>
2017-06-14 14:45:32 +0200

    chore: maj deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -x
 find .
 
 sshpass -e scp -vv target/*.jar vivatech@vivatech.pintade.org:/home/vivatech/
-sshpass -e scp -vv run.sh vivatech@vivatech.pintade.org:/home/vivatech/
-sshpass -e ssh -vv vivatech@vivatech.pintade.org ./run.sh
+#sshpass -e scp -vv run.sh vivatech@vivatech.pintade.org:/home/vivatech/
+sshpass -e ssh -vv vivatech@vivatech.pintade.org sudo systemctl restart vivatech
