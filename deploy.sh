0779644b1d0a7138df28d8df64550d1dddd24241
Christiansen MEREL <christiansen.merel@gmail.com>
2017-06-14 12:21:11 +0200

    chore: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-env
-
 set -x
 
-sshpass -e scp target/*.jar vivatech@vivatech.pintade.org:/home/vivatech/
-sshpass -e scp run.sh vivatech@vivatech.pintade.org:/home/vivatech/
-sshpass -e ssh vivatech@vivatech.pintade.org ./run.sh
+find .
+
+sshpass -e scp -vv target/*.jar vivatech@vivatech.pintade.org:/home/vivatech/
+sshpass -e scp -vv run.sh vivatech@vivatech.pintade.org:/home/vivatech/
+sshpass -e ssh -vv vivatech@vivatech.pintade.org ./run.sh
